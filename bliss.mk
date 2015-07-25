6fac3bd487d29f7f34d9501b51c81ce5ca6d6b05
Patrick Graber <pjgraber03@gmail.com>
2015-07-24 17:39:26 -0700

    Update and rename cm.mk to bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -1,10 +1,13 @@
+#Bliss Audio Mod
+BLISS_AUDIO_MOD := viper
+
 $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltexx \
@@ -13,5 +16,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOB7 release-keys"
 
 PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
+PRODUCT_NAME := bliss_jfltexx
 
